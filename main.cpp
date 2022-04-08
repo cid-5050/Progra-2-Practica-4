839f35179a9140908d2fcc5d76da72ce635bc9bf
Cid <59629922+cid-5050@users.noreply.github.com>
2022-04-08 16:19:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+template <typename T>
+class ClaseTemplatizada {
+  public:
+    ClaseTemplatizada();
+    ClaseTemplatizada(const ClaseTemplatizada &);
+
+  private:
+
+};
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
